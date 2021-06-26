a550428aa30326846a80487e3eff66f2d6e945c5
mdarwinj382000 <mdarwinj@gmail.com>
2021-06-27 01:12:09 +0530

    ghappy added tested

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,29 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    GHappy gh;
+
+    @BeforeEach
+    public void initialize() {
+        this.gh = new GHappy();
+    }
+
+    @Test
+    public void ghappytest1() {
+        boolean result = gh.gHappy("xxggxx");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void ghappytest2(){
+        Assertions.assertFalse(gh.gHappy("xxgxx"));
+    }
+    @Test
+    public void ghappytest3(){
+        Assertions.assertFalse(gh.gHappy("xxggyygxx"));
+    }
+
 }
