5775a7dcb45b5903cf26784ce13abac829adb783
mdarwinj382000 <mdarwinj@gmail.com>
2021-06-27 01:41:20 +0530

    Ceaser added tested

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,17 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class CaesarShiftCipherTest {
+    @ParameterizedTest(name = "{0} : {2}={1}")
+    @CsvSource({
+            "'abc','def',3", "'xyz','abc',3", "'xyz','bcd',4"
+    })
+    public void ceasertester(String input,String expected,int shift) {
+        CaesarShiftCipher cph = new CaesarShiftCipher();
+        String result = cph.CaesarShiftCipher(input, shift);
+        Assertions.assertEquals(expected, result);
+    }
 }
